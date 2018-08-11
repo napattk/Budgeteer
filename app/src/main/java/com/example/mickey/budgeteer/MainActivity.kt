f4972932babf4e9a6c3aab476bdedc30ea05ffba
Mickey <napatt_K@hotmail.com>
2018-08-11 13:37:26 +0100

    Added total change field to main activity

--- app/src/main/java/com/example/mickey/budgeteer/MainActivity.kt
+++ app/src/main/java/com/example/mickey/budgeteer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mickey.budgeteer
 
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -14,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Calculate total change
+        var dbHandler = DBHandler(this)
+        var budgetItems = dbHandler.readData()
+        var totalChange = 0;
+
+        budgetItems.iterator().forEach {
+            when(it.type){
+                "Expense" -> totalChange = totalChange - it.amount!!;
+                "Income" -> totalChange = totalChange + it.amount!!;
+            }
+        }
+
+
+        if(totalChange < 0){
+            changeAmountText.setTextColor(Color.parseColor("#8C2A2A"))
+            changeAmountText.setText("-"+totalChange)
+            changeImage.setImageResource(R.drawable.decline)
+        }else{
+            changeAmountText.setTextColor(Color.parseColor("#587C12"))
+            changeAmountText.setText("+"+totalChange)
+            changeImage.setImageResource(R.drawable.increase)
+        }
+
+        //Set-up items list
         budgetList.layoutManager = LinearLayoutManager(this);
         budgetList.adapter = BudgetViewAdapter(this);
 
