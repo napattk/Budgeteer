d1cb87d9a69e4f9e32b5fd97388749f2aead7d2d
Mickey <napatt_K@hotmail.com>
2018-08-15 12:12:04 +0100

    Added summary view activity, add new item activity: change field color, change constraints

---
+++ app/src/main/java/com/example/mickey/budgeteer/SummaryViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mickey.budgeteer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SummaryViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary_view)
+    }
+}
